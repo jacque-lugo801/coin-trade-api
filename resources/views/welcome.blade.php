36a114eff2fc5f3ee1acdb2b34df950315e95774
“jacque-lugo801” <jacque.lugo801@gmail.com>
2024-01-08 13:33:35 -0600

    Testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -118,6 +118,10 @@
                     </div>
                 </div>
 
+                <div>
+                    necesita material, primengs
+                </div>
+
                 <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
                         <div class="flex items-center gap-4">
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+/*
 Route::get('/', function () {
+    return view('welcome');
+});
+*/
+Route::get('/welcome', function () {
     return view('welcome');
 });
